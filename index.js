942e2dc1d6634d21c09aedd28284fc3c510df3fa
sunga-lee <sungajunior1@gmail.com>
2023-03-24 20:39:42 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
+function findMatching(drivers,name){
+     
+    let element = drivers.filter(function(element){
+        return element.toLowerCase() === name.toLowerCase()
+        
+    })
+    return element
+
+    
+}
+function matchName(driver,name){
+    let element = driver.filter(function(element){
+        return element.name === name
+    })
+    return element
+}
+
+function fuzzyMatch(driver,name){
+    let element = driver.filter(function(element){
+        return element.substring(0,2) === name
+        
+    })
+    return element
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Code your solution here
